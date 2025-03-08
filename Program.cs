2d0d1b34d5c75df347c2a2a440188db94cda5e90
JyotiUSA <jyotijain.usa@gmail.com>
2025-03-08 01:23:24 -0500

    Basic Student class was ready with single object 'jyoti'.  Showing results perfectly according to marks and percentage.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using School;
+
+Console.WriteLine("-------------Students--------------");
+Console.WriteLine("-----------------------------------");
+
+Student jyoti = new Student("Jyoti" , 6 , 27);
+
+jyoti.DisplayStudentsDetails();
+
+jyoti.Exame();
+jyoti.Result();
+
+jyoti.Exame(100, 100, 100, 100);
+jyoti.Result();
+
+jyoti.Exame(35, 35, 35, 35);
+jyoti.Result();
+
+jyoti.Exame(15, 25, 5, 35);
+jyoti.Result();
+
+jyoti.Exame(85, 84, 85, 76);
+jyoti.Result();
+
+jyoti.Result(false);
+
+
+Console.ReadLine();
---
+++ Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class Student
+    {
+        public int rollNo;
+        public string name;
+        public int std;
+
+        public string[] subjects;
+
+        public int marks;
+        public double percentage;
+        public bool resultStatus;
+
+        const int minimunmarks = 1;
+
+        public Student(string studentName, int studentStd, int studentRollNo)
+        {
+            rollNo = studentRollNo;
+            name = studentName;
+            std = studentStd;
+        }
+
+        public void Exame()
+        {
+            //enter the marks obtain in exame
+            //Console.WriteLine($"\n {name} \t\t has obtain total marks :\t\t {marks}"); //200
+            Exame(minimunmarks,minimunmarks,minimunmarks, minimunmarks);
+        }
+
+        public void Exame(int mathsMarks, int languageMarks, int socialScienceMarks, int scienceMarks)
+        {
+            //enter the marks obtain in each subject
+            marks = (mathsMarks + languageMarks + scienceMarks + scienceMarks);
+            Console.WriteLine($"\n{name}\t\thas obtain total marks :\t\t{marks}"); //50+50+50+50=200
+        }
+
+        public bool Result( bool absentInExame = true)
+        {
+            percentage = marks * 100  / 400  ; // (200/400)*100 = 0.5 * 100 = 50 %
+
+            Console.WriteLine($"maks --- {marks}");
+            Console.WriteLine($"percentage --- {percentage}");
+
+
+            if (percentage >= 35)
+            {
+                resultStatus = true;
+                Console.WriteLine($"\n{name}\t\tin pass in the Examination.");   //Pass
+            }
+            else
+            {
+                resultStatus = false;
+                Console.WriteLine($"\n{name}\t\tis fail in the Examination.");   //Fail
+            }
+
+            if (absentInExame)
+                marks = 0;
+
+            return resultStatus;
+        }
+
+        public void DisplayStudentsDetails()
+        {
+            Console.WriteLine($"\nName:\t\t{name}\nStd.:\t\t{std}\nRoll\tno.:\t\t{rollNo}");
+        }
+    }
+}
