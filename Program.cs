466108b0f264497d91c673c563d0821a154886ed
JyotiUSA <jyotijain.usa@gmail.com>
2025-03-11 13:31:52 -0400

    two objects runnig well with all functions. Still can impove as - creating new classes for sports section, exame section, etc. Still some unit testing errors exists.

--- Program.cs
+++ Program.cs
@@ -1,28 +1,62 @@
 ﻿using School;
 
+//string jyotiAsJson = jyoti.ConvertToJson();
+//Console.WriteLine(jyotiAsJson);
+
 Console.WriteLine("-------------Students--------------");
 Console.WriteLine("-----------------------------------");
 
-Student jyoti = new Student("Jyoti" , 6 , 27);
+//Static variable use for sportsmarks, because those are standard marks for all students9i.e., object of school class.)
+Student.sportMarks = 20;
 
+#region Jyoti Object
+//Object Jyoti is created and defined
+Student jyoti = new Student("Jyoti" , 6 , 27, new DateTime(1993,09,16) );
+
+//Detailos of jyoti
 jyoti.DisplayStudentsDetails();
 
-jyoti.Exame();
-jyoti.Result();
+//Exame marks , will decide are your pass or fail
+jyoti.Exame(60, 20, 37, 90);
 
-jyoti.Exame(100, 100, 100, 100);
-jyoti.Result();
+//All about sports participants
+jyoti.calculateSportsMarks();
 
-jyoti.Exame(35, 35, 35, 35);
-jyoti.Result();
+// Now Winner will wiiner of 6th std. will get 25 marks in sports if they win.
+//We are using "ref". To specific std. students, the marks changes now not default marks.
+int newSportWinnerMarksFor6Std = 25; 
+jyoti.calculateSportsMarksForWinners(ref newSportWinnerMarksFor6Std);
 
-jyoti.Exame(15, 25, 5, 35);
-jyoti.Result();
+//Calculating Percentages.
+jyoti.Result(); //jyoti was present in exame, so no absent need to mention in percentage calculation.
 
-jyoti.Exame(85, 84, 85, 76);
-jyoti.Result();
+Console.WriteLine($"Thank you {jyoti.name}.....");
 
-jyoti.Result(false);
+#endregion
 
+#region Tejeshree Onject
+//Object Tejeshree is created and defined
+Student tejeshree = new Student("Tejeshree", 12, 53, new DateTime(1981, 08, 24));
+
+//Detailos of tejeshree
+tejeshree.DisplayStudentsDetails();
+
+//Exame marks , will decide are your pass or fail
+tejeshree.Exame(91, 15, 30, 25);
+
+//All about sports participants
+tejeshree.calculateSportsMarks();
+
+// Now Winner will wiiner of 6th std. will get 25 marks in sports if they win.
+//We are using "ref". To specific std. students, the marks changes now not default marks.
+int newSportWinnerMarksFor12Std = 30;
+tejeshree.calculateSportsMarksForWinners(ref newSportWinnerMarksFor12Std);
+
+//Calculating Percentages.
+tejeshree.Result(); //jyoti was present in exame, so no absent need to mention in percentage calculation.
+
+Console.WriteLine($"Thank you {tejeshree.name}.....");
+
+#endregion
 
 Console.ReadLine();
--- Student.cs
+++ Student.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,64 +13,117 @@ namespace School
         public int rollNo;
         public string name;
         public int std;
+        public DateTime birthDate;
 
         public string[] subjects;
 
         public int marks;
         public double percentage;
         public bool resultStatus;
+        public string passOrFail;
+
+        public static int concessionMarks = 35;
+
+        public static int sportMarks = 5;
 
         const int minimunmarks = 1;
 
-        public Student(string studentName, int studentStd, int studentRollNo)
+        public Student(string studentName, int studentStd, int studentRollNo, DateTime studentbirthDate)
         {
             rollNo = studentRollNo;
             name = studentName;
             std = studentStd;
+            birthDate = studentbirthDate;
         }
 
-        public void Exame()
+        public double Exame(bool absentInExame = false)
         {
-            //enter the marks obtain in exame
-            //Console.WriteLine($"\n {name} \t\t has obtain total marks :\t\t {marks}"); //200
-            Exame(minimunmarks,minimunmarks,minimunmarks, minimunmarks);
-        }
-
-        public void Exame(int mathsMarks, int languageMarks, int socialScienceMarks, int scienceMarks)
-        {
-            //enter the marks obtain in each subject
-            marks = (mathsMarks + languageMarks + scienceMarks + scienceMarks);
-            Console.WriteLine($"\n{name}\t\thas obtain total marks :\t\t{marks}"); //50+50+50+50=200
-        }
-
-        public bool Result( bool absentInExame = true)
-        {
-            percentage = marks * 100  / 400  ; // (200/400)*100 = 0.5 * 100 = 50 %
-
-            Console.WriteLine($"maks --- {marks}");
-            Console.WriteLine($"percentage --- {percentage}");
-
-
-            if (percentage >= 35)
-            {
-                resultStatus = true;
-                Console.WriteLine($"\n{name}\t\tin pass in the Examination.");   //Pass
-            }
-            else
-            {
-                resultStatus = false;
-                Console.WriteLine($"\n{name}\t\tis fail in the Examination.");   //Fail
-            }
-
             if (absentInExame)
+            {
                 marks = 0;
+                Console.WriteLine("You was absent in exame.");
+            }
 
-            return resultStatus;
+            return Exame(minimunmarks,minimunmarks,minimunmarks, minimunmarks);
+        }
+
+        public double Exame(int mathsMarks, int languageMarks, int socialScienceMarks, int scienceMarks)
+        {
+            if ((mathsMarks <= 35) && (languageMarks >= 35 && socialScienceMarks >= 35 && scienceMarks >= 35))
+            {
+                mathsMarks = concessionMarks;
+                resultStatus = true;
+            }
+            else if ((languageMarks <= 35) && (mathsMarks >= 35 && socialScienceMarks >= 35 && scienceMarks >= 35))
+            {
+                languageMarks = concessionMarks;
+                resultStatus = true;
+            }
+            else if ((socialScienceMarks <= 35) && (languageMarks >= 35 && mathsMarks >= 35 && scienceMarks >= 35))
+            {
+                socialScienceMarks = concessionMarks;
+                resultStatus = true;
+            }
+            else if ((scienceMarks <= 35) && (languageMarks >= 35 && socialScienceMarks >= 35 && mathsMarks >= 35))
+            {
+                scienceMarks = concessionMarks;
+                resultStatus = true;
+            }
+            passOrFail = "Fail";
+            if (resultStatus)
+            {
+                passOrFail = "Pass";
+            }
+
+
+            //enter the marks obtain in each subject
+            marks = (mathsMarks + languageMarks + socialScienceMarks + scienceMarks);
+
+            Console.WriteLine($"\n{name}\thas obtain total marks :\t{marks}\n"); //50+50+50+50=200
+            Console.WriteLine($"\n{name}\tis\t{passOrFail}\tin this examination."); //50+50+50+50=200
+
+            return marks;
+
+        }
+
+        public double calculateSportsMarks()
+        {
+            marks = marks + sportMarks ;
+            Console.WriteLine($"\nAfter adding\t{sportMarks}\tas a standard Sport's participant marks, your total marks are:\t{marks}");
+            return marks;
+        }
+
+        public int calculateSportsMarksForWinners(ref int winnerSportsMarks)
+        {
+            winnerSportsMarks = 50;
+            marks = marks + winnerSportsMarks;
+            Console.WriteLine($"\nAs your a winner, you will get extra 50 marks. Now your total marks will be\t{marks}\t");
+            return marks;
+        }
+
+        public double Result()
+        {
+            percentage = (double)marks * 100  / 400  ; // (200/400)*100 = 0.5 * 100 = 50 %
+
+            Console.WriteLine($"\nYou achive\t{percentage}%\tin this examination.\n");
+
+            return percentage;
+        }
+
+        public static void UsingACustomType()
+        {
+            List<string> list = new List<string>();
+        }
+
+        public string ConvertToJson()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
         }
 
         public void DisplayStudentsDetails()
         {
-            Console.WriteLine($"\nName:\t\t{name}\nStd.:\t\t{std}\nRoll\tno.:\t\t{rollNo}");
+            Console.WriteLine($"\nName:\t{name}\nStd.:\t{std}\nRoll\tno.:\t{rollNo}\nBirth\tdate:\t{birthDate}");
         }
     }
 }
